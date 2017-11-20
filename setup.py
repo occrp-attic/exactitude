a8d548c902fbfb4edf3b5003fdc0c8ab225a9c77
Friedrich Lindenberg <friedrich@pudo.org>
2017-11-20 09:18:49 +0100

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='exactitude',
-    version='2.1.0',
+    version='2.1.1',
     description="A library with real-world data parsers.",
     long_description="",
     classifiers=[
