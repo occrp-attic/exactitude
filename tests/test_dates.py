4992273f61e73c754565371fedbef1812d245622
rhiaro <amy@rhiaro.co.uk>
2018-02-19 18:37:52 +0000

    Restore commented test

--- tests/test_dates.py
+++ tests/test_dates.py
@@ -58,9 +58,8 @@ class DatesTest(unittest.TestCase):
         self.assertEquals(dates.clean(None), None)
         self.assertEquals(dates.clean(''), None)
         self.assertEquals(dates.clean('2017-04-04'), '2017-04-04')
-        # self.assertEquals(parse_date('2017-4-4'), '2017-04-04')
+        self.assertEquals(dates.clean('2017-4-4'), '2017-04-04')
 
-        # I dunno why the first one fails (gets caught by parsedatetime and parsed as today's date) but the second one passes (gets skipped by parsedatetime and dateutil handles it okay)
         self.assertEquals(dates.clean('23-AUG-2001'), '2001-08-23')
         self.assertEquals(dates.clean('09-jun-1993'), '1993-06-09')
 
