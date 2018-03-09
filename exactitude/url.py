06595e1a31140de24a737157cb62aa0a9d19b5f8
Friedrich Lindenberg <friedrich@pudo.org>
2018-03-09 06:33:40 -0600

    handle crashy URLs

--- exactitude/url.py
+++ exactitude/url.py
@@ -1,6 +1,9 @@
+import logging
 from urlnormalizer import normalize_url, is_valid_url
 
 from exactitude.common import ExactitudeType
+
+log = logging.getLogger(__name__)
 
 
 class UrlType(ExactitudeType):
@@ -11,4 +14,8 @@ class UrlType(ExactitudeType):
 
     def clean_text(self, url, **kwargs):
         """Perform intensive care on URLs, see `urlnormalizer`."""
-        return normalize_url(url)
+        try:
+            return normalize_url(url)
+        except UnicodeDecodeError:
+            log.warning("Invalid URL: %r" url)
+            
